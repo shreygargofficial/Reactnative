0e1b8a4f9e653f1fb0ae1fe64b425e3815c00d55
Shrey Garg <shreygarg@Shreys-MacBook-Air.local>
2024-09-20 18:16:41 +0530

    restart from here

--- App.js
+++ App.js
@@ -1,50 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, ActivityIndicator, Button, Pressable, FlatList } from 'react-native';
-import GoalInput from './Components/GoalInput';
-import GoalList from './Components/GoalList';
+import {Text, SafeAreaView} from 'react-native'
+
 
 export default function App() {
-  const [goal, setGoal] = useState('');
-  const [goalList, setList] = useState([]);
-
-  let inputChangeHandler = (value) => {
-    setGoal(value)
-  }
-  let addGoalHandler = () => {
-    if (goal && !goalList.includes(goal)) {
-      let newList = [...goalList];
-      newList.push(goal);
-      setList(newList)
-      setGoal('')
-    }
-
-  }
-
-  let deleteGoalHandler = (myGoalToDelete)=>{
-    if(myGoalToDelete){
-      setList(currentList=>{
-        return currentList.filter(goals=>goals!==myGoalToDelete)
-      })
-    }
-  }
-
 
   return (
     <>
-    <View style={styles.container}>
-      <GoalInput inputChangeHandler={inputChangeHandler} goal={goal} addGoalHandler={addGoalHandler}/>
-      <GoalList goalList={goalList} deleteGoalHandler={deleteGoalHandler}/>   
-
-    </View>
+    <StatusBar style='dark'/>
+    <SafeAreaView style={{}}>
+        <Text>Hello</Text>
+    </SafeAreaView>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding:10
-  },
 
-});
