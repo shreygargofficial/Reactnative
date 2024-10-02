c7b817b6dd640ce2f16f39fdf08487d5587657c3
Shrey Garg <shreygarg@Shreys-MacBook-Air.local>
2024-10-03 03:42:45 +0530

    optimised

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import {Text, SafeAreaView} from 'react-native'
+import { Text, SafeAreaView } from 'react-native'
 
 
 export default function App() {
 
   return (
     <>
-    <StatusBar style='dark'/>
-    <SafeAreaView style={{}}>
+      <StatusBar style='dark' />
+      <SafeAreaView style={{}}>
         <Text>Hello</Text>
-    </SafeAreaView>
+      </SafeAreaView>
     </>
   );
 }
